60fb09ebfdca7f1ec35ad2131befe2ffeeb78d28
TheDereck05 <952350113ed@gmail.com>
2025-08-22 08:05:45 -0500

    Ordenar carpetas

---
+++ Semana_2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int suma_de_enteros( int, int);
+int suma_de_enteros( int a, int b){
+return a+b;
+}
+int main(){
+    int a, b, resultado;
+    cout<< "Ingrese el valor de a:";
+    cin>>a;
+    cout<< "Ingrese el valor de b:";
+    cin>>b;
+    resultado = suma_de_enteros(a,b);
+    cout<< "La suma de:"<< a << "mas la suma de"<< b <<"es"<<resultado;
+    return 0;
+}
+
